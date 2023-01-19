baa83e9288ddbe55780332cac3dda98613a7e650
Ayo Sam <ayotolusam@gmail.com>
2023-01-19 13:20:11 +0100

    Building Users Table

--- src/components/DashboardUsersTable/DashboardUsersTable.tsx
+++ src/components/DashboardUsersTable/DashboardUsersTable.tsx
@@ -1,5 +1,7 @@
+import { useMemo } from 'react'
 import { useTable } from 'react-table';
 import './DashboardUsersTable.scss'
+import { icons } from '../../constants';
 import { Iuser } from '../../constants/types'
 
 type tableProps = {
@@ -8,11 +10,88 @@ type tableProps = {
 
 const DashboardUsersTable = ({data}: tableProps) => {
   
+  const filter = () => {
+    return (
+      <img src={icons.filter} alt="filter" />
+    )
+  }
+
+  const tableData = useMemo(() => [...data], [data]);
+
+  const columns = useMemo(() => [
+    {
+      Header: `ORGANIZATION ${filter}`,
+      accessor: "orgName"
+    },
+    {
+      Header: `USERNAME ${filter}`,
+      accessor: "userName"
+    },
+    {
+      Header: `EMAIL ${filter}`,
+      accessor: "email"
+    },
+    {
+      Header: `PHONE NUMBER ${filter}`,
+      accessor: "phoneNumber"
+    },
+    {
+      Header: `DATE JOINED ${filter}`,
+      accessor: "createdAt"
+    },
+    {
+      Header: `STATUS ${filter}`,
+      accessor: "inactive"
+    },
+  ], []);
+
+  const tableInstance = useTable({ columns, tableData })
+
+  const { 
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,  
+  } = tableInstance;
+  
   return (
-    <div>
-      {data[0].profile.firstName}
-    </div>
+    <table {...getTableProps()}>
+     <thead>
+       {
+       headerGroups.map( headerGroup => (
+         <tr {...headerGroup.getHeaderGroupProps()}>
+           {
+           headerGroup.headers.map( column => (
+             <th {...column.getHeaderProps()}>
+               {
+               column.render('Header')}
+             </th>
+           ))}
+         </tr>
+       ))}
+     </thead>
+     <tbody {...getTableBodyProps()}>
+       {
+       rows.map(row => {
+         prepareRow(row)
+         return (
+           <tr {...row.getRowProps()}>
+             {
+             row.cells.map(cell => {
+               return (
+                 <td {...cell.getCellProps()}>
+                   {
+                   cell.render('Cell')}
+                 </td>
+               )
+             })}
+           </tr>
+         )
+       })}
+     </tbody>
+   </table>
   )
 }
 
-export default DashboardUsersTable
+export default DashboardUsersTable;
